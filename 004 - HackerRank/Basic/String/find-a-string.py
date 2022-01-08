958007785b106fa88fed5905c15b70a66454ed91
ridhaginanjar <ridhaginanjar7@gmail.com>
2022-01-08 22:35:50 +0700

    add findstring case

---
+++ 004 - HackerRank/Basic/String/find-a-string.py
@@ -0,0 +1,13 @@
+def count_substring(string, sub_string):
+    num = 0
+    for i in range(len(string)) :
+        if string[i:].startswith(sub_string) :
+            num += 1
+    return num
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
